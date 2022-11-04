cc8b7c72950a2f0d0712d63d28cbc1974c018f22
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-04 22:20:08 +0700

    add Quantity and UnitCost type

--- scratch.fsx
+++ scratch.fsx
@@ -1,3 +1,9 @@
+type Quantity = Quantity of float
+type UnitCost = UnitCost of float
+
 /// Cost of BoQItem = (Quantity of Item * Material Unit Cost) + (Quantity of Item * Labor Unit Cost)
-let calcItemCost qty materialUnitCost laborUnitCost =
+let calcItemCost (Quantity qty) (UnitCost materialUnitCost) (UnitCost laborUnitCost) =  
   qty * materialUnitCost + qty * laborUnitCost
+
+let testCalcItemCost = 
+  (calcItemCost (Quantity 10) (UnitCost 100) (UnitCost 50)) = 1500
