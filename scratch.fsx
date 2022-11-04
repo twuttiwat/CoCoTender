e8e846c5c17436b219ef4059a5deba5deba3127d
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-04 22:14:54 +0700

    add scratch file for modeling

---
+++ scratch.fsx
@@ -0,0 +1,3 @@
+// Cost of BoQItem = (Quantity of Item * Material Unit Cost) + (Quantity of Item * Labor Unit Cost)
+let calcItemCost qty materialUnitCost laborUnitCost =
+  qty * materialUnitCost + qty * laborUnitCost
