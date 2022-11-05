0027f28320ce822f7fb7f463bd5736e4b33684b6
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-05 13:41:30 +0700

    add CRUD operation for BoQItem

--- scratch.fsx
+++ scratch.fsx
@@ -31,9 +31,39 @@ module BoQItem =
         MaterialUnitCost = materialUnitCost
         LaborUnitCost = laborUnitCost
         TotalCost = cost
-      }
-    ) 
-    
+      })
+
+  let updateDesc newDesc item = { item with Description = newDesc }
+
+  let tryUpdateQty newQty item = 
+    tryCalcItemCost newQty item.MaterialUnitCost item.LaborUnitCost
+    |> Option.map (fun cost -> 
+      {
+        item with
+          Quantity = newQty
+          TotalCost = cost
+      })
+
+  let tryUpdateMaterialUnitCost (Material newUnitCost) item =
+    let newMaterialUnitCost = Material newUnitCost
+    tryCalcItemCost item.Quantity newMaterialUnitCost item.LaborUnitCost
+    |> Option.map (fun cost -> 
+      {
+        item with
+          MaterialUnitCost = newMaterialUnitCost
+          TotalCost = cost
+      })
+
+  let tryUpdateLaborUnitCost (Labor newUnitCost) item =
+    let newLaborUnitCost = Labor newUnitCost
+    tryCalcItemCost item.Quantity item.MaterialUnitCost newLaborUnitCost 
+    |> Option.map (fun cost -> 
+      {
+        item with
+          LaborUnitCost = newLaborUnitCost
+          TotalCost = cost
+      })
+
 ///
 /// Test
 /// 
@@ -50,6 +80,12 @@ let testCalcItemCostError =
   let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "cm^2" }
   tryCalcItemCost (Quantity (10, "m^2")) materialUnitCost laborUnitCost = None 
 
+let poolItem =
+  let qty = Quantity (10, "m^2")
+  let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
+  let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "m^2" }
+  tryCreate "Pool Tile"  qty materialUnitCost laborUnitCost
+ 
 let testCreateBoQItem =
   let qty = Quantity (10, "m^2")
   let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
@@ -58,4 +94,11 @@ let testCreateBoQItem =
   boqItem
   |> Option.map (fun item -> item.TotalCost = 1500)
   |> Option.defaultValue false
-  
+
+let testUpdateDesc = 
+  poolItem 
+  |> Option.map (updateDesc "New Pool") 
+  |> Option.map (fun x -> x.Description = "New Pool") 
+  |> Option.defaultValue false
+
+
