2c2b4106deb93ada88b1a94d9fcd96c7d15076ec
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-05 22:14:59 +0700

    add more test for BoQItem CRUD

--- scratch.fsx
+++ scratch.fsx
@@ -101,4 +101,25 @@ let testUpdateDesc =
   |> Option.map (fun x -> x.Description = "New Pool") 
   |> Option.defaultValue false
 
+let testUpdateQty = 
+  let newQty = Quantity (20,"m^2")
+  poolItem 
+  |> Option.bind (tryUpdateQty  newQty) 
+  |> Option.map (fun x -> x.TotalCost = 3000) 
+  |> Option.defaultValue false
 
+let testUpdateMaterialUnitCost = 
+  let newMaterialUnitCost = 
+    Material <| { Name = "Small Tile"; UnitCost = 50; Unit = "m^2"}
+  poolItem 
+  |> Option.bind (tryUpdateMaterialUnitCost  newMaterialUnitCost) 
+  |> Option.map (fun x -> x.TotalCost = 1000) 
+  |> Option.defaultValue false
+
+let testUpdateLaborUnitCost = 
+  let newLaborUnitCost = 
+    Labor <| { Name = "Do Tiling #2"; UnitCost = 100; Unit = "m^2"}
+  poolItem 
+  |> Option.bind (tryUpdateLaborUnitCost  newLaborUnitCost) 
+  |> Option.map (fun x -> x.TotalCost = 2000) 
+  |> Option.defaultValue false
