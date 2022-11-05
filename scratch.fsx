5a0b8968250293866adff948e04f0a6ef9ae24bb
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-05 13:06:17 +0700

    add type BoQItem

--- scratch.fsx
+++ scratch.fsx
@@ -1,23 +1,46 @@
 type Quantity = Quantity of float*string
 type UnitCost = 
   {
-    Name: string
-    UnitCost: float
-    Unit: string
+    Name : string
+    UnitCost : float
+    Unit : string
   }
 type MaterialUnitCost = Material of UnitCost
 type LaborUnitCost = Labor of UnitCost
 
-/// Calcuate cost of BoQItem
-let tryCalcItemCost (Quantity (qty,qtyUnit)) (Material materialUnitCost) (Labor laborUnitCost) =  
-  if qtyUnit = materialUnitCost.Unit && qtyUnit = laborUnitCost.Unit then 
-    (qty * materialUnitCost.UnitCost + qty * laborUnitCost.UnitCost) |> Some
-  else
-    None
+type BoQItem =
+  {
+    Description : string
+    Quantity : Quantity
+    MaterialUnitCost : MaterialUnitCost
+    LaborUnitCost : LaborUnitCost
+    TotalCost : float
+  }
+module BoQItem =
+  let tryCalcItemCost (Quantity (qty,qtyUnit)) (Material materialUnitCost) (Labor laborUnitCost) =  
+    if qtyUnit = materialUnitCost.Unit && qtyUnit = laborUnitCost.Unit then 
+      (qty * materialUnitCost.UnitCost + qty * laborUnitCost.UnitCost) |> Some
+    else
+      None
 
+  let tryCreate desc qty materialUnitCost laborUnitCost =
+    tryCalcItemCost qty materialUnitCost laborUnitCost
+    |> Option.map (fun cost -> 
+      {
+        Description = desc
+        Quantity = qty
+        MaterialUnitCost = materialUnitCost
+        LaborUnitCost = laborUnitCost
+        TotalCost = cost
+      }
+    ) 
+    
 ///
 /// Test
 /// 
+
+open BoQItem
+
 let testCalcItemCost = 
   let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
   let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "m^2" }
@@ -27,3 +50,13 @@ let testCalcItemCostError =
   let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m" }
   let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "cm^2" }
   tryCalcItemCost (Quantity (10, "m^2")) materialUnitCost laborUnitCost = None 
+
+let testCreateBoQItem =
+  let qty = Quantity (10, "m^2")
+  let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
+  let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "m^2" }
+  let boqItem = tryCreate "Pool Tile"  qty materialUnitCost laborUnitCost
+  boqItem
+  |> Option.map (fun item -> item.TotalCost = 1500)
+  |> Option.defaultValue false
+  
