697fa93d2c147ada75e48b17dffe39632df47d85
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-06 17:08:40 +0700

    add maybe ce

--- scratch.fsx
+++ scratch.fsx
@@ -1,3 +1,15 @@
+type MaybeBuilder() =
+
+  member this.Bind(x, f) =
+    match x with
+    | None -> None
+    | Some a -> f a
+
+  member this.Return(x) =
+    Some x
+
+let maybe = new MaybeBuilder()
+
 type Quantity = Quantity of float*string
 type UnitCost = 
   {
@@ -76,16 +88,6 @@ module BoQItem =
 
 open BoQItem
 
-let testCalcItemCost = 
-  let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
-  let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "m^2" }
-  tryCalcItemCost (Quantity (10, "m^2")) materialUnitCost laborUnitCost = Some 1500 
-
-let testCalcItemCostError = 
-  let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m" }
-  let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "cm^2" }
-  tryCalcItemCost (Quantity (10, "m^2")) materialUnitCost laborUnitCost = None 
-
 let poolItem =
   let qty = Quantity (10, "m^2")
   let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
@@ -93,39 +95,43 @@ let poolItem =
   tryCreate "Pool Tile"  qty materialUnitCost laborUnitCost
  
 let testCreateBoQItem =
-  let qty = Quantity (10, "m^2")
-  let materialUnitCost = Material { Name = "Big Tile"; UnitCost = 100; Unit = "m^2" }
-  let laborUnitCost = Labor { Name = "Do Tiling"; UnitCost = 50; Unit = "m^2" }
-  let boqItem = tryCreate "Pool Tile"  qty materialUnitCost laborUnitCost
-  boqItem
-  |> Option.map (fun item -> item.TotalCost = 1500)
+  maybe {
+    let! poolItem' = poolItem
+    return poolItem'.TotalCost = 1500
+  }
   |> Option.defaultValue false
 
 let testUpdateDesc = 
-  poolItem 
-  |> Option.map (updateDesc "New Pool") 
-  |> Option.map (fun x -> x.Description = "New Pool") 
+  maybe {
+    let! poolItem' = poolItem
+    let updatedPoolItem = updateDesc "New Pool" poolItem'
+    return updatedPoolItem.Description = "New Pool"
+  }
   |> Option.defaultValue false
 
 let testUpdateQty = 
-  let newQty = Quantity (20,"m^2")
-  poolItem 
-  |> Option.bind (tryUpdateQty  newQty) 
-  |> Option.map (fun x -> x.TotalCost = 3000) 
+  maybe {
+    let! poolItem' = poolItem
+    let newQty = Quantity (20,"m^2")
+    let! updatedPoolItem = tryUpdateQty newQty poolItem'
+    return updatedPoolItem.TotalCost = 3000
+  }
   |> Option.defaultValue false
 
 let testUpdateMaterialUnitCost = 
-  let newMaterialUnitCost = 
-    Material <| { Name = "Small Tile"; UnitCost = 50; Unit = "m^2"}
-  poolItem 
-  |> Option.bind (tryUpdateMaterialUnitCost  newMaterialUnitCost) 
-  |> Option.map (fun x -> x.TotalCost = 1000) 
+  maybe {
+    let! poolItem' = poolItem
+    let newMaterialUnitCost = Material <| { Name = "Small Tile"; UnitCost = 50; Unit = "m^2"}
+    let! updatedPoolItem = tryUpdateMaterialUnitCost newMaterialUnitCost poolItem'
+    return updatedPoolItem.TotalCost = 1000
+  }
   |> Option.defaultValue false
 
 let testUpdateLaborUnitCost = 
-  let newLaborUnitCost = 
-    Labor <| { Name = "Do Tiling #2"; UnitCost = 100; Unit = "m^2"}
-  poolItem 
-  |> Option.bind (tryUpdateLaborUnitCost  newLaborUnitCost) 
-  |> Option.map (fun x -> x.TotalCost = 2000) 
+  maybe { 
+    let! poolItem' = poolItem
+    let newLaborUnitCost = Labor <| { Name = "Do Tiling #2"; UnitCost = 100; Unit = "m^2"}
+    let! updatedPoolItem = tryUpdateLaborUnitCost newLaborUnitCost poolItem'
+    return updatedPoolItem.TotalCost = 2000
+  }
   |> Option.defaultValue false
