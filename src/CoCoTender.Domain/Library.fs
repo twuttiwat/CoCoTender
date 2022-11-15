1d5e04793fa490a026d233bde911c9775dc90e4c
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-15 13:27:56 +0700

    create tests when creating boq

--- src/CoCoTender.Domain/Library.fs
+++ src/CoCoTender.Domain/Library.fs
@@ -3,3 +3,62 @@
 module Say =
     let hello name =
         $"Hello, {name}"
+
+
+type Quantity = Quantity of float*string
+type UnitCost = 
+  {
+    Name : string
+    UnitCost : float
+    Unit : string
+  }
+type MaterialUnitCost = Material of UnitCost
+type LaborUnitCost = Labor of UnitCost
+
+type BoQItem = private {
+    Description : string
+    Quantity : Quantity
+    MaterialUnitCost : MaterialUnitCost
+    LaborUnitCost : LaborUnitCost
+    TotalCost : float
+  }
+
+module BoQItem =
+  let calcCost qty unitCost =
+    match qty, unitCost with
+    | Quantity (qty, qtyUnit), {UnitCost = unitCost; Unit = unitCostUnit} 
+      when qtyUnit = unitCostUnit ->
+        (qty * unitCost) |> Some
+    | _ -> None
+
+  let tryCalcItemCost qty (Material materialUnitCost) (Labor laborUnitCost) =  
+    match calcCost qty materialUnitCost, calcCost qty laborUnitCost with
+    | Some materialCost, Some laborCost -> Some (materialCost + laborCost)
+    | _ -> None
+
+  let isValid desc qty materialUnitCost laborUnitCost =
+    match qty, materialUnitCost, laborUnitCost with
+    | Quantity (_, qtyUnit), Material {Unit = materialUnit}, Labor {Unit = laborUnit} ->
+      qtyUnit = materialUnit && qtyUnit = laborUnit  
+
+  let tryCreate desc qty materialUnitCost laborUnitCost = 
+    if isValid desc qty materialUnitCost laborUnitCost  then 
+      tryCalcItemCost qty materialUnitCost laborUnitCost
+      |> Option.map(fun cost -> {
+          Description = desc
+          Quantity = qty
+          MaterialUnitCost = materialUnitCost
+          LaborUnitCost = laborUnitCost
+          TotalCost = cost
+        })
+    else
+      None
+
+  let value item = 
+    {| 
+      Description = item.Description
+      Quantity = item.Quantity
+      MaterialUnitCost = item.MaterialUnitCost
+      LaborUnitCost = item.LaborUnitCost
+      TotalCost = item.TotalCost
+    |}
--- tests/CoCoTender.DomainTests/Tests.fs
+++ tests/CoCoTender.DomainTests/Tests.fs
@@ -9,3 +9,43 @@ open CoCoTender.Domain
 [<Fact>]
 let ``Hello test`` () =
     Say.hello "world" |> should equal "Hello, world"
+
+module ``Create boq item`` =
+
+    let desc = "Pool Item"
+    let qty = Quantity (10.0, "m^2")
+    let material = Material { Name = "Big Tile"; UnitCost = 100.0; Unit = "m^2" }
+    let labor = Labor { Name = "Do Tiling"; UnitCost = 50.0; Unit = "m^2" }
+    let totalCost = 10*100 + 10*50
+
+    [<Fact>]
+    let ``should succeed if Quantity, Material and Labor use the same Unit`` () = 
+        let result = BoQItem.tryCreate desc qty material labor
+        match result with
+        | Some item -> 
+            let itemVal = item |> BoQItem.value
+            itemVal.Description |> should equal desc
+            itemVal.Quantity |> should equal qty
+            itemVal.MaterialUnitCost |> should equal material
+            itemVal.LaborUnitCost |> should equal labor
+        | None -> Assert.Fail "Could not create BoQItem"
+
+    [<Fact>]
+    let ``should fail if Quantity, Material and Labor have different Units`` () = 
+        let material' = material |> fun (Material m) -> {m with Unit = "m"} |> Material
+        let labor' = labor |> fun (Labor l) -> {l with Unit = "cm"} |> Labor
+
+        let result = BoQItem.tryCreate desc qty material' labor'
+        match result with
+        | None -> Assert.True(true)
+        | Some _ -> Assert.Fail "Should not create BoQItem with different Unit"        
+
+    [<Fact>]
+    let ``should always calculate total cost when create`` () = 
+        let result = BoQItem.tryCreate desc qty material labor
+        match result with
+        | Some item -> 
+            let itemVal = item |> BoQItem.value
+            itemVal.TotalCost |> should equal totalCost
+        | None -> Assert.Fail "Could not create BoQItem"
+
