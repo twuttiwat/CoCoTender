26cba13e2d0b435267533c6453e8d521a6b75bb7
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-15 12:05:36 +0700

    try unit testing and console

--- src/CoCoTender.Console/Program.fs
+++ src/CoCoTender.Console/Program.fs
@@ -1,2 +1,5 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open CoCoTender
+open CoCoTender.Domain
+
+Say.hello "world"
+|> printfn "%s" 
--- tests/CoCoTender.DomainTests/Tests.fs
+++ tests/CoCoTender.DomainTests/Tests.fs
@@ -2,7 +2,10 @@ module Tests
 
 open System
 open Xunit
+open FsUnit
+open CoCoTender
+open CoCoTender.Domain
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Hello test`` () =
+    Say.hello "world" |> should equal "Hello, world"
