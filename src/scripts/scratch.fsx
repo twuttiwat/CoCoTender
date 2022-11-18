d16d8fb012d7095ef19aecf11ebb946d82bd4ea0
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2022-11-18 12:44:03 +0700

    try result with creating boq and estimate

--- src/scripts/scratch.fsx
+++ src/scripts/scratch.fsx
@@ -1,92 +1,200 @@
-#r "nuget: FSharp.Data, 5.0.2"
-#r @"C:\projs\CoCoTender\src\CoCoTender.Domain\bin\Debug\net6.0\CoCoTender.Domain.dll"
+#r "nuget:FsToolkit.ErrorHandling"
 
-open FSharp.Data
-open CoCoTender.Domain
+open FsToolkit.ErrorHandling
+
+type Quantity = Quantity of float*string
+type UnitCost = 
+  {
+    Name : string
+    UnitCost : float
+    Unit : string
+  }
+type MaterialUnitCost = Material of UnitCost
+type LaborUnitCost = Labor of UnitCost
+
+type BoQItem = private {
+    Description : string
+    Quantity : Quantity
+    MaterialUnitCost : MaterialUnitCost
+    LaborUnitCost : LaborUnitCost
+    TotalCost : float
+  }
+
+module BoQItem =
+  let calcCost qty unitCost =
+    match qty, unitCost with
+    | Quantity (qty, qtyUnit), {UnitCost = unitCost; Unit = unitCostUnit} 
+      when qtyUnit = unitCostUnit ->
+        (qty * unitCost) |> Some
+    | _ -> None
+
+  let tryCalcItemCost qty (Material materialUnitCost) (Labor laborUnitCost) =  
+    match calcCost qty materialUnitCost, calcCost qty laborUnitCost with
+    | Some materialCost, Some laborCost -> Some (materialCost + laborCost)
+    | _ -> None
+
+  let calcItemCost qty (Material materialUnitCost) (Labor laborUnitCost) =  
+    match calcCost qty materialUnitCost, calcCost qty laborUnitCost with
+    | Some materialCost, Some laborCost -> Ok (materialCost + laborCost)
+    | _ -> Error "Could not calculate item cost"
+
+  let isValid desc qty materialUnitCost laborUnitCost =
+    match qty, materialUnitCost, laborUnitCost with
+    | Quantity (_, qtyUnit), Material {Unit = materialUnit}, Labor {Unit = laborUnit} ->
+      qtyUnit = materialUnit && qtyUnit = laborUnit  
+
+  let areUnitsMatched  qty materialUnitCost laborUnitCost =
+    match qty, materialUnitCost, laborUnitCost with
+    | Quantity (_, qtyUnit), Material {Unit = materialUnit}, Labor {Unit = laborUnit} ->
+      qtyUnit = materialUnit && qtyUnit = laborUnit
+
+  let create desc qty materialUnitCost laborUnitCost = result {
+     
+    do! areUnitsMatched qty materialUnitCost laborUnitCost |> Result.requireTrue "Unit are Not matched." 
+
+    let! itemCost =  calcItemCost qty materialUnitCost laborUnitCost
+
+    return {
+      Description = desc
+      Quantity = qty
+      MaterialUnitCost = materialUnitCost
+      LaborUnitCost = laborUnitCost
+      TotalCost = itemCost
+    }
+  }
+
+
+  let tryCreate desc qty materialUnitCost laborUnitCost = 
+    if isValid desc qty materialUnitCost laborUnitCost  then 
+      tryCalcItemCost qty materialUnitCost laborUnitCost
+      |> Option.map(fun cost -> {
+          Description = desc
+          Quantity = qty
+          MaterialUnitCost = materialUnitCost
+          LaborUnitCost = laborUnitCost
+          TotalCost = cost
+        })
+    else
+      None
+
+  let value item = 
+    {| 
+      Description = item.Description
+      Quantity = item.Quantity
+      MaterialUnitCost = item.MaterialUnitCost
+      LaborUnitCost = item.LaborUnitCost
+      TotalCost = item.TotalCost
+    |}
+
+  let updateDesc newDesc item = { item with Description = newDesc }
+
+  let tryUpdateQty newQty item = 
+    tryCalcItemCost newQty item.MaterialUnitCost item.LaborUnitCost
+    |> Option.map (fun cost -> 
+      {
+        item with
+          Quantity = newQty
+          TotalCost = cost
+      })
+
+  let tryUpdateMaterialUnitCost (Material newUnitCost) item =
+    let newMaterialUnitCost = Material newUnitCost
+    tryCalcItemCost item.Quantity newMaterialUnitCost item.LaborUnitCost
+    |> Option.map (fun cost -> 
+      {
+        item with
+          MaterialUnitCost = newMaterialUnitCost
+          TotalCost = cost
+      })
+
+  let tryUpdateLaborUnitCost (Labor newUnitCost) item =
+    let newLaborUnitCost = Labor newUnitCost
+    tryCalcItemCost item.Quantity item.MaterialUnitCost newLaborUnitCost 
+    |> Option.map (fun cost -> 
+      {
+        item with
+          LaborUnitCost = newLaborUnitCost
+          TotalCost = cost
+      })
+
+module Project =
+
+  open BoQItem
+
+  type DirectCost = DirectCost of float
+  type FactorFTable = FactorFTable of (float*float) list
+
+  let calcDirectCost items = 
+    items 
+    |> List.sumBy (fun a -> a |> value |> (fun b -> b.TotalCost))
+    |> DirectCost
+
+  let calcBoundF  lowerBound upperBound directCost =
+    let lowerBoundCost,lowerBoundF = lowerBound
+    let upperBoundCost,upperBoundF = upperBound
+    let fRange = upperBoundF - lowerBoundF
+    let costRatio = (directCost - lowerBoundCost) / (upperBoundCost - lowerBoundCost)
+    costRatio * fRange + lowerBoundF
+
+  let (|LessThanMin|GreaterThanMax|BetweenRange|) input =
+    let (FactorFTable fTable),(DirectCost cost) = input
+    let minCost, minF = (fTable |> List.head)
+    let maxCost, maxF = (fTable |> List.last)
+    if cost < minCost then LessThanMin(minF)
+    else if cost > maxCost then GreaterThanMax(maxF)
+    else
+      let lowerBoundIndex = fTable |> List.findIndexBack (fun (cost',_) -> cost > cost')
+      let upperBoundIndex = lowerBoundIndex + 1
+      BetweenRange(calcBoundF (List.item lowerBoundIndex fTable) (List.item upperBoundIndex fTable) cost)
+
+  let calcFactorF fTable directCost =
+    match (fTable,directCost) with
+    | LessThanMin f -> f
+    | GreaterThanMax f -> f
+    | BetweenRange f -> f
+     
+  let applyFactorF loadFactorFTable (DirectCost directCost) =
+    let factorF = calcFactorF (loadFactorFTable()) (DirectCost directCost)
+    directCost * factorF
+
+  // Strip value after thousands
+  let roundCost  = function 
+    | a when a < 1000.0 -> a
+    | b -> 
+      b / 1000.0  
+      |> System.Math.Truncate
+      |> (*) 1000.0
+
+  let estimateCost' loadFactorFTable = calcDirectCost >> (applyFactorF loadFactorFTable) >> roundCost 
+
+  let estimateCost loadFactorFTable items : Result<float,string> = 
+    let cost = estimateCost' loadFactorFTable items
+    Ok cost
+
 open BoQItem
 open Project
 
-[<Literal>]
-let ResolutionFolder = __SOURCE_DIRECTORY__
+let desc = "Pool Item"
+let qty = Quantity (10.0, "m^2")
+let material = Material { Name = "Big Tile"; UnitCost = 100.0; Unit = "m^2" }
+let labor = Labor { Name = "Do Tiling"; UnitCost = 50.0; Unit = "m^2" }
+let totalCost = 10*100 + 10*50
 
-type BoQItems = CsvProvider<"boq.csv", ResolutionFolder=ResolutionFolder>
-let testBoQ = BoQItems.Load(__SOURCE_DIRECTORY__ + "/boq.csv")
+let item = 
+  match BoQItem.create desc qty material labor with
+  | Ok item' -> item'
+  | _ -> failwith "Could not create item for updating"
 
-let q = Quantity (1.0, "u")
-let u = { Name = "n"; Unit = "u"; UnitCost = 1.0}
-let m = Material u
-let l = Labor u
+let itemDiffUnits = 
+  let newMaterial, newLabor = 
+    match material,labor with 
+    | Material m, Labor lb ->  Material { m with Unit = "m"},  Labor {lb with Unit = "cm"}
 
-BoQItem.tryCreate "a" q m l
+  match BoQItem.create desc qty newMaterial newLabor with
+  | Ok item' -> false
+  | Error msg -> printfn "%s" msg; true
 
-let (firstRow:BoQItems.Row) = testBoQ.Rows |> Seq.head
+let loadFactorFTableFn = 
+  function () -> FactorFTable [(10,1.1); (100,1.5); (1000, 1.9)]
 
-let toBoQItem (row:BoQItems.Row) =
-  let qty = Quantity (row.Quantity, row.Unit.Trim())
-  let material = 
-    {
-        Name = row.Material
-        Unit = row.MaterialUnit.Trim()
-        UnitCost = row.MaterialUnitCost
-    }
-    |> Material
-
-  let labor = 
-    {
-      Name = row.Labor
-      Unit = row.LaborUnit.Trim()
-      UnitCost = row.LaborUnitCost
-    }
-    |> Labor
-
-  BoQItem.tryCreate row.Description qty material labor
-
-let directCost =
-  testBoQ.Rows 
-  |> Seq.choose toBoQItem 
-  |> Seq.sumBy (fun x -> x |> value |> fun y -> y.TotalCost)
-  |> DirectCost
-
-open Project
-
-type FactorFFile = CsvProvider<"FactorF.csv", ResolutionFolder=ResolutionFolder>
-let testFactorF = FactorFFile.Load(__SOURCE_DIRECTORY__ + "/FactorF.csv")
-let fRows = testFactorF.Rows
-let fTable = fRows |> Seq.map (fun x -> x.DirectCost,x.FactorF) |> List.ofSeq |> FactorFTable
-
-let loadBoQFile (filePath:string) =
-  BoQItems
-    .Load(__SOURCE_DIRECTORY__ + "/boq.csv")
-    .Rows
-  |> Seq.choose toBoQItem 
-  |> List.ofSeq
-
-let loadFactorFFile (filePath:string) () = 
-  FactorFFile
-    .Load(filePath)
-    .Rows
-  |> Seq.map (fun x -> x.DirectCost,x.FactorF) 
-  |> List.ofSeq 
-  |> FactorFTable
-
-let boqFile = __SOURCE_DIRECTORY__ + "/boq.csv"
-let factorFFile = __SOURCE_DIRECTORY__ + "/FactorF.csv"
-let cost = estimateCost (loadFactorFFile factorFFile) (loadBoQFile boqFile) 
-
-printfn "%A" fsi.CommandLineArgs
-match fsi.CommandLineArgs |> List.ofArray with 
-| _::boqFile::factorFFile::_ -> 
-  let cost = estimateCost (loadFactorFFile factorFFile) (loadBoQFile boqFile) 
-  printfn $"Estimate Cost is {cost}"
-| _::_ -> 
-  let boqFile = __SOURCE_DIRECTORY__ + @"\boq.csv"
-  let factorFFile = __SOURCE_DIRECTORY__ + @"\FactorF.csv"
-  printfn "%s" boqFile
-  printfn "%s" factorFFile
-  let cost = estimateCost (loadFactorFFile factorFFile) (loadBoQFile boqFile) 
-  printfn $"Estimate Cost is {cost}"
-| _ -> 
-  printfn "Should not be here"
-
-open System.IO
-Directory.GetCurrentDirectory()
+estimateCost loadFactorFTableFn [item] 
